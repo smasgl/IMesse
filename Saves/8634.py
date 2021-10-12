2b9cfe709f8853ea0de71958b32e909f2e81e1e3
smasgl <masooglu.simon@gmail.com>
2021-10-12 14:09:14 +0100

    Fixed lastscript

--- Saves/8634.py
+++
@@ -1,8 +0,0 @@
-import os, time, threading
-def sub_func(): os.system(f"cd ~ && python3 Documents/GitHub/IMesse/FlatWorldRestart.py")
-threading.Thread(target=sub_func).start()
-time.sleep(5)
-from mcpi.minecraft import Minecraft
-mc = Minecraft.create()
-mc.postToChat(f"Key: {os.path.basename(__file__)[:4]}")
-
--- SavesLastProject.py
+++
@@ -1,2 +0,0 @@
-import os
-os.system(f"thonny /home/pi/Documents/GitHub/IMesse/Saves/4414.py")
