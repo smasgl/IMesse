de9441c282a8a81d1b04e4d148bbdf2e75c58f39
lwnmas9 <masooglu.simon@gmail.com>
2021-10-13 10:13:18 +0200

    deleted unwanted lines

--- iMesseStartup.py
+++ iMesseStartup.py
@@ -6,9 +6,6 @@ from random import randint
 saves = r'/home/pi/Documents/GitHub/IMesse/Saves'
 iMesse = r'/home/pi/Documents/GitHub/IMesse'
 
-#os.system(f"xdg-open /usr/share/raspi-ui-overrides/applications/minecraft-pi.desktop")
-#time.sleep(0.5)
-#os.system(f"python3 {iMesse}/CloseWindows.py")
 os.system(f"rm {iMesse}/SavesLastProject.py")
 randomKey = randint(1000, 9999)
 while os.path.exists(f"{randomKey}.py"):
@@ -17,7 +14,7 @@ f = open(f"{saves}/{randomKey}.py", "w")
 f.write("import os, time, threading\n")
 f.write(f'def sub_func(): os.system(f"cd ~ && python3 Documents/GitHub/IMesse/FlatWorldRestart.py")\n')
 f.write(f'threading.Thread(target=sub_func).start()\n')
-f.write("time.sleep(5)\n")
+f.write("time.sleep(6)\n")
 f.write(f'from mcpi.minecraft import Minecraft\n')
 f.write(f'mc = Minecraft.create()\n')
 f.write('mc.postToChat(f"Key: {os.path.basename(__file__)[:4]}")\n\n')
