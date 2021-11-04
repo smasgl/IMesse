05a836058a0e65d1cfba575ad552affed54492ed
Netzer <masooglu.simon@libherr.com>
2021-11-04 10:01:39 +0100

    Added pycraft library

--- Beispiel.py
+++ Beispiel.py
@@ -1,37 +1,31 @@
-import os, time, threading
-def sub_func(): os.system(f"cd ~ && python3 Documents/GitHub/IMesse/FlatWorldRestart.py")
-threading.Thread(target=sub_func).start()
-time.sleep(6)
-from mcpi.minecraft import Minecraft
-from mcpi import block
-mc = Minecraft.create()
-mc.postToChat(f"Dies ist ein Beispiel Skript.")
+import pycraft
 
-#Beispiel Code
-#Boden Bauen:
-mc.setBlocks(-5, -1, -5, 5, -1, 5, block.WOOL.id)
-#Linke Wand
-mc.setBlocks(-5, -1, -5, -5, 2, 5, block.WOOD_PLANKS.id)
-#Rechte Wand
-mc.setBlocks(5, -1, -5, 5, 2, 5, block.WOOD_PLANKS.id)
-#Vordere Wand
-mc.setBlocks(-5, -1, -5, 5, 2, -5, block.WOOD_PLANKS.id)
-#Hintere Wand
-mc.setBlocks(-5, -1, 5, 5, 2, 5, block.WOOD_PLANKS.id)
-#Türe
-mc.setBlock(0, 0, -5, 64, 0)
-mc.setBlock(0, 1, -5, 64, 8)
-#Dach
-for x in range(6):
-    mc.setBlocks(0-x,8-x,0-x,0+x,8-x,0+x, 98, 0)
-#Vordere Fenster
-mc.setBlocks(-2,1,-5,-3,1,-5, block.GLASS.id)
-mc.setBlocks(2,1,-5,3,1,-5, block.GLASS.id) 
-#Linkes Fenster
-mc.setBlocks(-5,1,3,-5,1,-3, block.GLASS.id)
-#Rechtes Fenster
-mc.setBlocks(5,1,3,5,1,-3, block.GLASS.id)
-#Hinteres Fenster
-mc.setBlocks(-3,1,5,3,1,5, block.GLASS.id) 
+pycraft.initialize()
 
 
+# # Beispiel Code
+# # Boden Bauen:
+# mc.setBlocks(-5, -1, -5, 5, -1, 5, block.WOOL.id)
+# # Linke Wand
+# mc.setBlocks(-5, -1, -5, -5, 2, 5, block.WOOD_PLANKS.id)
+# # Rechte Wand
+# mc.setBlocks(5, -1, -5, 5, 2, 5, block.WOOD_PLANKS.id)
+# # Vordere Wand
+# mc.setBlocks(-5, -1, -5, 5, 2, -5, block.WOOD_PLANKS.id)
+# # Hintere Wand
+# mc.setBlocks(-5, -1, 5, 5, 2, 5, block.WOOD_PLANKS.id)
+# # Türe
+# mc.setBlock(0, 0, -5, 64, 0)
+# mc.setBlock(0, 1, -5, 64, 8)
+# # Dach
+# for x in range(6):
+#     mc.setBlocks(0 - x, 8 - x, 0 - x, 0 + x, 8 - x, 0 + x, 98, 0)
+# # Vordere Fenster
+# mc.setBlocks(-2, 1, -5, -3, 1, -5, block.GLASS.id)
+# mc.setBlocks(2, 1, -5, 3, 1, -5, block.GLASS.id)
+# # Linkes Fenster
+# mc.setBlocks(-5, 1, 3, -5, 1, -3, block.GLASS.id)
+# # Rechtes Fenster
+# mc.setBlocks(5, 1, 3, 5, 1, -3, block.GLASS.id)
+# # Hinteres Fenster
+# mc.setBlocks(-3, 1, 5, 3, 1, 5, block.GLASS.id)
---
+++ pycraft.py
@@ -0,0 +1,20 @@
+import os
+import threading
+import time
+from mcpi import block
+from mcpi.minecraft import Minecraft
+
+
+def restart_flat_world_script():
+    os.system(f"cd ~ && python3 Documents/GitHub/IMesse/FlatWorldRestart.py")
+
+
+def initialize():
+    threading.Thread(target=restart_flat_world_script).start()
+    time.sleep(6)
+
+    mc = Minecraft.create()
+    mc.postToChat(f"Dies ist ein Beispiel Skript.")
+
+
+
